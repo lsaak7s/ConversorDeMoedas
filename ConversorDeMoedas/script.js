042d49be582362946cf68568b25eca7c17019130
IsaacAlves <isaacsantos.7r@gmail.com>
2026-06-27 14:46:12 -0300

    JS outras moedas

--- ConversorDeMoedas/script.js
+++ ConversorDeMoedas/script.js
@@ -1,6 +1,7 @@
-const converterButton = document.getElementById("converterButton");
-
 function convertervalues() {
+
+    const converterButton = document.getElementById("converterButton");
+    const Selectpaiz = document.getElementById("Selectpaiz")
 
     const resultadovalor = document.getElementById("resultadovalor");
     const resultadofinal = document.getElementById("resultadofinal");
@@ -8,24 +9,43 @@ function convertervalues() {
     const inseriValor = document.getElementById("inseriValor").value
 
     const dolar = 5.2;
-    const euro = 7.5;
-    const libra = 9.0;
+    const libra = 7.5;
+    const euro = 9.0;
 
-    const valorEmDolar = inseriValor / dolar
+    if (Selectpaiz.value == "dolar") {
+        resultadofinal.innerHTML = new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD"
+        }).format(inseriValor / dolar)
+    }
 
+    if (Selectpaiz.value == "euro") {
+        resultadofinal.innerHTML = new Intl.NumberFormat("de-DE", {
+            style: "currency",
+            currency: "EUR"
+        }).format(inseriValor / euro)
+    }
+
+    
+        if (Selectpaiz.value == "libra") {
+            resultadofinal.innerHTML = new Intl.NumberFormat("lb", {
+                style: "currency",
+                currency: "GBP"
+            }).format(inseriValor / libra)
+        }
+    
     resultadovalor.innerHTML = new Intl.NumberFormat("pt-BR", {
         style: "currency",
         currency: "BRL"
     }).format(inseriValor)
 
-    resultadofinal.innerHTML = new Intl.NumberFormat("en-US", {
-        style: "currency",
-        currency: "USD"
-    }).format(valorEmDolar)
-    
-   
+    console.log(Selectpaiz.value)
+    console.log(resultadofinal)
 
-    console.log(valorEmDolar)
+
+
+
+
 }
 
 
