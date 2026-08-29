9217f36a7b2e4c825907064ff26af57043b18c8f
Isaac Alves <Isaacsantos.7r@gmail.com>
2026-08-29 18:30:55 -0300

    Segundo selcte feito

--- ConversorDeMoedas/script.js
+++ ConversorDeMoedas/script.js
@@ -1,12 +1,12 @@
-/*Se Precisa ter interação Guarde em uma Variável */
+//Se Precisa ter interação Guarde em uma Variável
 const primeiroSelect = document.getElementById("primeiroSelect1");
-/*Primeira moeda*/
+//Primeira moeda
 const primeiraImg = document.getElementById("primeiraImg");
 const moedav1 = document.getElementById("moedav1");
-/*Ele estar olhando 👀*/
+//Ele estar olhando 👀
 primeiroSelect.addEventListener("change", imgvalueUser1);
 
-/*Muda a primeira imagem e o texto da moeda */
+//Muda a primeira imagem e o texto da moeda 
 function imgvalueUser1() {
 
     if (primeiroSelect.value == "dolar") {
@@ -27,16 +27,16 @@ function imgvalueUser1() {
     }
 }
 
-/*Segunda moeda */
+//Segunda moeda 
 const segundoSelect = document.getElementById("segundoSelect2");
 
-/*Segunda moeda*/
+//Segunda moeda
 const segundaImg = document.getElementById("segundaImg");
 const moedav2 = document.getElementById("moedav2");
-/*Ele estar olhando 👀*/
+//Ele estar olhando 👀
 
 segundoSelect.addEventListener("change", imgvalueUser2);
-/*Muda a segunda imagem e o texto da moeda */
+//Muda a segunda imagem e o texto da moeda
 
 function imgvalueUser2() {
 
@@ -57,48 +57,70 @@ function imgvalueUser2() {
         segundaImg.src = "./assets/BR.png"
     }
 }
-
-/*
-const real = data.BTCBRL.high
-const dolar = data.USDBRL.high
-const euro = data.EURBRL.high
-const libra = data.GBPBRL.high
-
-*/
-/*BUTTON*/
+//BUTTON
 const buttonConverter = document.getElementById("buttonConverter");
 buttonConverter.addEventListener("click", convertervalues);
-/*INPUT Valor do User*/
-const inseriValor = document.getElementById("inseriValor")
-/*Função de conversão de valores */
-async function convertervalues() {
 
-    const data = await fetch("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL,GBP-BRL").then(response => response.json())
-    console.log(data)
-    
+//INPUT Valor do User
+const inseriValor = document.getElementById("inseriValor")
+
+//Função de conversão de valores
+async function convertervalues() {
+    //live values
+    const data = await fetch("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,GBP-BRL,BRL-USD").then(response => response.json())
+
     const taxas = {
-        real: data.BTCBRL.high,
+        real: data.BRLUSD.high,
         dolar: data.USDBRL.high,
         euro: data.EURBRL.high,
         libra: data.GBPBRL.high
     }
-    
+
+    console.log(taxas);
+
     /*Pega o valor do input e transforma em número */
     const valor = Number(inseriValor.value);
     /*Pega o valor dos select e transforma em string */
     const origem = primeiroSelect.value;
     const destino = segundoSelect.value;
 
-    /*Transforma o valor em reais */
+    //Transforma o valor em reais
     const valorEmReal = valor * taxas[origem];
-    /*Dá o resultado da conversão */
-    const resultado = valorEmReal / taxas[destino];
-
-    /*Mostra o valor do input e o resultado da conversão */
+    //Dá o resultado da conversão
+    const resultado = valor / taxas[destino];
+    //Mostra o valor do input e o resultado da conversão
     const valueinUser = document.getElementById("valueinUser")
     const valueFinal = document.getElementById("valueFinal")
-    /*Vai amostra inserido*/
-    valueinUser.innerHTML = valor;
+
+
+    //Valor Formatado do Usuario
+
+    if (origem === "real") {
+        valueinUser.innerHTML = new Intl.NumberFormat("pt-BR", {
+            style: "currency",
+            currency: "BRL"
+        }).format(valor)
+    }
+    if (origem === "dolar") {
+        valueinUser.innerHTML = new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD"
+        }).format(valor)
+    }
+    if (origem === "euro") {
+        valueinUser.innerHTML = new Intl.NumberFormat("de-DE", {
+            style: "currency",
+            currency: "EUR"
+        }).format(valor)
+    }
+    if (origem === "libra") {
+        valueinUser.innerHTML = new Intl.NumberFormat("en-GB", {
+            style: "currency",
+            currency: "GBP"
+        }).format(valor)
+    }
+
+    //Valor Formatado do Resultado
 
     if (destino === "real") {
         valueFinal.innerHTML = new Intl.NumberFormat("pt-BR", {
@@ -123,7 +145,9 @@ async function convertervalues() {
             style: "currency",
             currency: "GBP"
         }).format(resultado)
-    }/*Use isso para chamar a função toda vez que tiver uma alteração */
+    }
+
+    //Use isso para chamar a função toda vez que tiver uma alteração 
     primeiroSelect.addEventListener("change", convertervalues);
     segundoSelect.addEventListener("change", convertervalues);
 }
